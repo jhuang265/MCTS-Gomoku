e4c61c78816d22ca47ef8147a12d4cdbc92cab7a
Jerry Huang <jerryhuang265@hotmail.com>
2019-10-27 11:09:34 -0400

    edit environment

--- human_ui.py
+++ human_ui.py
@@ -1,4 +1,4 @@
-from gomoku_env import GomokuEnv
+from gomoku_env_history import GomokuEnv
 from mcts_uct import MCTS
 import numpy as np
 
@@ -16,7 +16,7 @@ COLUMN = {"a": 0, "b": 1, "c": 2,
           "D": 3, "E": 4, "F": 5,
           "G": 6, "H": 7, "I": 8}
 
-SIMULATIONS = 10000
+SIMULATIONS = 50000
 GAMES = 2
 
 
